bf60467e07788a257d45aecdd7410c87b1a04a21
Sam J <sam.jiang@karoshealth.com>
2013-03-04 19:11:19 -0500

    added script to benchmark for overhead
    also beefed up basictest a little

--- benchs/basictest.m
+++ benchs/basictest.m
@@ -1,18 +1,44 @@
 function ret = basictest()
 
-% test single call
-helperfunc(0);
-
+% test single call (jit compilation time)
 tic;
+helperfunc(0);
+t_compilation = toc;
+
+% test plain call many times (non recursive)
+tic;
+for i = 1:5000000
+  helperfunc(0);
+end
+t_simple_5000k = toc;
+
+% recursive calls
+tic;
+for i = 1:50000
+  helperfunc(100);
+end
+t_simple_50k_r100 = toc;
+
+
+% call embedded with matrix stuff
 ret = rand(100);
 
-for i = 1:1000
+tic;
+
+for i = 1:2000
   ret = ret * ret;
-  % test call in loops
-  helperfunc(2);
+  helperfunc(10);
 end
 
-t = toc;
-disp(['time ', num2str(t)]);
+t_mixed_2k_r10 = toc;
 
+newline = sprintf('\n');
+
+% display times
+disp([newline, ...
+  'TIMING_compilation:', num2str(t_compilation), newline, ...
+  'TIMING_simple_5000k:', num2str(t_simple_5000k), newline, ...
+  'TIMING_simple_50k_r100:', num2str(t_simple_50k_r100), newline, ...
+  'TIMING_mixed_r10:', num2str(t_mixed_2k_r10), newline, ...
+  ]);
 end
---
+++ benchs/run.py
@@ -0,0 +1,115 @@
+#!/usr/bin/python
+
+
+# author: Sam J
+# usage: ./run matlabfile
+
+# the matlab file should print to stdout its timings in the format of 
+# TIMING_name: seconds
+# where name can be anything without spaces, and seconds can be a float
+# (leading and trailing white spaces are trimmed)
+# 
+
+#############################
+
+import sys
+import os
+import subprocess as sp
+import numpy
+
+if len(sys.argv) < 2:
+  raise Exception("Please specify test file to execute")
+
+iterations = 5
+
+testfile = sys.argv[1]
+
+
+#mcvm doesn't like the .m extension
+if testfile.endswith('.m'):
+  testfile = testfile[:-2]
+
+mcvm = '../mcvm'
+mcvmstock = '../mcvmstock'
+
+if not os.access(mcvm, os.X_OK):
+  raise Exception("Cannot find executable " + mcvm)
+
+if not os.access(mcvmstock, os.X_OK):
+  raise Exception("Cannot find executable " + mcvmstock)
+
+timings = {}
+timings_stock = {}
+
+def bench(executable, testfile, ret):
+  print("Executing test " + testfile + " using " + executable)
+  p = sp.Popen([executable, testfile], stdout=sp.PIPE) # don't capture stdout
+
+  while True:
+    for line in p.stdout.readlines():
+      line = line.strip()
+
+      colon = line.find(':')
+
+      if colon < 0 or (not line.startswith('TIMING_')):
+        continue
+
+      name = line[ len('TIMING_') : colon ];
+      time = float(line[colon+1:])
+
+      if name in ret:
+        ret[name].append(time)
+      else:
+        ret[name] = [time]
+
+    if p.poll() is not None:
+      break
+
+
+  print("Found timings: " + str(ret.keys()))
+
+
+def stats(lst):
+  return [min(lst), max(lst), numpy.mean(lst), numpy.std(lst)]
+
+"""
+does t1 - t2 element wise, with percentage
+"""
+def diff(t1, t2):
+  assert len(t1) == len(t2)
+  ret = []
+  for i in range(len(t1)):
+    delta = t1[i] - t2[i]
+    percentage = 0 if t2[i] == 0 else delta / t2[i]
+    ret.append( [delta, percentage] )
+ 
+  return ret
+
+        
+for i in range(iterations):
+  print("Iteration: " + str(i+1))
+  bench(mcvm, testfile, timings)
+  bench(mcvmstock, testfile, timings_stock)
+
+names = timings.keys()
+names.sort()
+for name in names:
+  stock = stats(timings_stock[name])
+  new = stats(timings[name])
+
+  comp = diff(new, stock)
+
+  print("Timing: " + name)
+
+  print("  Stock: mean %.2f\t\tmin %.2f\t\tmax %.2f\t\tstd %.2f" % 
+      (stock[2], stock[0], stock[1], stock[3]))
+  print("  New:   mean %.2f\t\tmin %.2f\t\tmax %.2f\t\tstd %.2f" % 
+      (new[2], new[0], new[1], new[3]))
+
+  print("  Diff:  mean %.2f(%.2f%%)\tmin %.2f(%.2f%%)\
+      \tmax %.2f(%.2f%%)\t\tstd %.2f(%.2f%%)" % 
+      (comp[2][0], 100*comp[2][1], 
+       comp[0][0], 100*comp[0][1], 
+       comp[1][0], 100*comp[1][1], 
+       comp[3][0], 100*comp[3][1]))
+
