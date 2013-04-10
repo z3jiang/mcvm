2bdc10ead556a1fd903cef37346ed01e7a98e3b4
Sam J <sam.jiang@karoshealth.com>
2013-04-10 12:15:50 -0400

    updated basic loop test

--- benchs/basiclooptest.m
+++ benchs/basiclooptest.m
@@ -7,6 +7,13 @@ for i=1:1000000000
 end
 t_empty_loop = toc;
 
+tic;
+for i=1:100000
+  for j=1:10000
+  end
+end
+t_nested_loop = toc;
+
 M = rand(5);
 for i=1:1000000
   M = M*M;
@@ -17,6 +24,7 @@ t_matmult_loop = toc;
 
 disp([newline,...
   'TIMING_empty_loop: ', num2str(t_empty_loop), newline,...
+  'TIMING_nested_loop: ', num2str(t_nested_loop), newline,...
   'TIMING_matmult_loop: ', num2str(t_matmult_loop), newline,...
   newline]);
 
